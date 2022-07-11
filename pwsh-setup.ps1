90e4aa8c6745f04e2b014b3fe7697fe2631e1804
Jean-Michel Perraud <per202@csiro.au>
2022-07-11 21:28:34 +1000

    try powershell task

---
+++ pwsh-setup.ps1
@@ -0,0 +1,11 @@
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+# https://www.powershellgallery.com/packages/Invoke-MsBuild/2.7.1
+Install-Module -Name Invoke-MsBuild -Scope CurrentUser
+
+$githubRepoDir=$env:BUILD_SOURCESDIRECTORY
+$Path = ($githubRepoDir + "\s\config-utils\automation\PSBatchBuild")
+# Install-Module -Scope CurrentUser -Name PSScriptAnalyzer -force
+# Invoke-ScriptAnalyzer -Path $Path
+Import-Module -Name $Path -Verbose 
