a9fe584b00b8403da0e00b99dc96ef9bf748bbf0
J-M <jean-michel.perraud@csiro.au>
2022-07-12 16:02:16 +1000

    Trial setting up vcpp props file

--- make-props.ps1
+++ make-props.ps1
@@ -3,8 +3,10 @@ $OUR_SRC_DIR = Join-Path $env:BUILD_SOURCESDIRECTORY 's'
 
 $target_file = Join-Path $env:USERPROFILE 'vcpp_config.props'
 
-((Get-Content -path Join-Path $env:BUILD_SOURCESDIRECTORY 'vcpp_config.props.template' -Raw) -replace 'ROOT_SRC_DIR',$OUR_SRC_DIR) | Set-Content -Path $target_file
+$template_file = Join-Path $env:BUILD_SOURCESDIRECTORY 'vcpp_config.props.template'
 
 Write-Host $target_file
+
+((Get-Content -path $template_file -Raw) -replace 'ROOT_SRC_DIR', $OUR_SRC_DIR) | Set-Content -Path $target_file
 
 Get-Content -path $target_file | Write-Host
