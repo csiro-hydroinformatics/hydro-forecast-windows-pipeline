62eff2049671beb893475453e720d5738fad542a
J-M <jean-michel.perraud@csiro.au>
2022-07-25 18:52:47 +1000

    WIP Fetch artifact from the linux build

--- build-r-pkgs.bat
+++ build-r-pkgs.bat
@@ -9,6 +9,20 @@ if not defined root_src_dir (
     goto exit
 ) 
 
+if not defined linux_packages_dir ( 
+    set error_msg=ERROR: linux_packages_dir not defined
+    set exit_code=1
+    goto exit
+)
+
+set tarball_dir=%linux_packages_dir%\swift_setup\swift_setup\latest\r_pkgs\lib\R_repo\src\contrib
+
+if not exist %tarball_dir% ( 
+    set error_msg=ERROR: %tarball_dir% does not exist
+    set exit_code=1
+    goto exit
+)
+
 set csiro_dir=%root_src_dir%
 set cruise_control_dir=%csiro_dir%\cruise-control
 set cc_script_dir=%cruise_control_dir%\scripts
@@ -36,15 +50,91 @@ call %cc_script_dir%\setup_dev Default 64
 REM avoid issue writing to program installed library.
 set rlib_dos=%userprofile%\Rlib  
 if not exist %rlib_dos% mkdir %rlib_dos% 
-set rlib_nix=%rlib_dos:\=/%          
-set R_LIBS_SITE=%rlib_nix%                                  
+set rlib_nix=%rlib_dos:\=/%
+set R_LIBS_SITE=%rlib_nix%
 
-set RCMD_BUILD_OPT_NO_VIGNETTE=--no-build-vignettes
-@REM call %cc_script_dir%\build_r_packages.cmd %RCMD_BUILD_OPT_NO_VIGNETTE%
-call %cc_script_dir%\build_r_packages.cmd
+REM if not calling build_r_packages.cmd:
+%R_SCRIPT% %csiro_dir%\cruise-control\scripts\setup_dependent_packages.r
+
+@REM set RCMD_BUILD_OPT_NO_VIGNETTE=--no-build-vignettes
+@REM @REM call %cc_script_dir%\build_r_packages.cmd %RCMD_BUILD_OPT_NO_VIGNETTE%
+@REM call %cc_script_dir%\build_r_packages.cmd
+@REM @if %errorlevel% neq 0 (
+@REM     set exit_code=%errorlevel%
+@REM     set error_msg=build_r_packages failed 
+@REM     goto exit
+@REM )
+
+REM #######################################################
+REM Build binary packages
+
+
+
+:: %R_VANILLA% CMD INSTALL --build %tarball_dir%\rClr_*.tar.gz
+:: %R_VANILLA% CMD INSTALL --build %tarball_dir%\capigen_*.tar.gz
+
+REM  percentage - tilda and fI        - expands %I to a fully qualified path name
+REM check "for /?" in DOS
+
+REM Let's be specific rather than bulk file pattern, just in case
+
+REM un-double the %% to % if you copy/paste to cmd rather than call this as a script. Don't blame me.
+REM Also, sorry for ugly redundancies. I dont trust to get Batch loops right yet: https://stackoverflow.com/questions/4334209/nested-batch-for-loops 
+
+set R_BUILD_CMD=%R_VANILLA% CMD INSTALL --build
+
+for %%I in ( %tarball_dir%\mhplot_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
 @if %errorlevel% neq 0 (
     set exit_code=%errorlevel%
-    set error_msg=build_r_packages failed 
+    set error_msg=Building R package mhplot failed
+    goto exit
+)
+for %%I in ( %tarball_dir%\cinterop_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
+@if %errorlevel% neq 0 (
+    set exit_code=%errorlevel%
+    set error_msg=Building R package cinterop failed
+    goto exit
+)
+for %%I in ( %tarball_dir%\msvs_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
+@if %errorlevel% neq 0 (
+    set exit_code=%errorlevel%
+    set error_msg=Building R package msvs failed
+    goto exit
+)
+for %%I in ( %tarball_dir%\uchronia_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
+@if %errorlevel% neq 0 (
+    set exit_code=%errorlevel%
+    set error_msg=Building R package uchronia failed
+    goto exit
+)
+for %%I in ( %tarball_dir%\joki_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
+@if %errorlevel% neq 0 (
+    set exit_code=%errorlevel%
+    set error_msg=Building R package joki failed
+    goto exit
+)
+for %%I in ( %tarball_dir%\swift_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
+@if %errorlevel% neq 0 (
+    set exit_code=%errorlevel%
+    set error_msg=Building R package swift failed
+    goto exit
+)
+for %%I in ( %tarball_dir%\calibragem_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
+@if %errorlevel% neq 0 (
+    set exit_code=%errorlevel%
+    set error_msg=Building R package calibragem failed
+    goto exit
+)
+for %%I in ( %tarball_dir%\qpp_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
+@if %errorlevel% neq 0 (
+    set exit_code=%errorlevel%
+    set error_msg=Building R package qpp failed
+    goto exit
+)
+for %%I in ( %tarball_dir%\efts_*.tar.gz ) do ( %R_BUILD_CMD% %%~fI )
+@if %errorlevel% neq 0 (
+    set exit_code=%errorlevel%
+    set error_msg=Building R package efts failed
     goto exit
 )
 
---
+++ fetch-pkgs.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# If az login or az devops login haven't been used, all az devops commands 
+# will try to sign in using a PAT stored in the AZURE_DEVOPS_EXT_PAT environment variable.
+# To use a PAT, set the AZURE_DEVOPS_EXT_PAT environment variable at the process level.
+
+if [ ! -z ${AZURE_DEVOPS_EXT_PAT+x} ]; then
+    echo "INFO variable AZURE_DEVOPS_EXT_PAT already defined";
+else
+    echo "ERROR env variable AZURE_DEVOPS_EXT_PAT must be defined";
+    exit 1
+fi
+
+wd=$1
+
+swift_current_version_dir=${wd}/swift_setup_version_current
+swift_setup_version_dir=${wd}/swift_setup_version
+swift_setup_dir=${wd}/swift_setup/latest
+
+mkdir -p $swift_current_version_dir
+mkdir -p $swift_setup_version_dir
+mkdir -p $swift_setup_dir
+
+if [ ! -f ${swift_current_version_dir}/version.txt ]; then
+    echo "0.0.1" > ${swift_current_version_dir}/version.txt
+fi
+
+# vercomp: license: cc-by-sa 
+# credits: https://stackoverflow.com/a/4025065
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+_download_version () {
+
+    az artifacts universal download --organization https://dev.azure.com/OD222236-DigWaterAndLandscapes --scope project --project "OD222236-DigWaterAndLandscapes" --feed "hydro_forecast_deb" --name "swift_deb_version" --version "*" --path ${swift_setup_version_dir}
+
+    if [ $? == 0 ]; then
+        return 0;
+    else
+        echo "FAILED: swift_setup_version download";
+        return 1;
+    fi
+}
+
+_download_setup () {
+
+    az artifacts universal download --organization https://dev.azure.com/OD222236-DigWaterAndLandscapes --scope project --project "OD222236-DigWaterAndLandscapes" --feed "hydro_forecast_deb" --name "swift_deb" --version "*" --path ${swift_setup_dir}
+
+    if [ $? == 0 ]; then
+        return 0;
+    else
+        echo "FAILED: swift_setup download";
+        return 1;
+    fi
+}
+
+_download_version
+if [ ! $? == 0 ]; then
+    exit 1;
+fi
+
+currentver=`cat ${swift_current_version_dir}/version.txt`
+newver=`cat ${swift_setup_version_dir}/version.txt`
+
+vercomp $currentver $newver
+case $? in
+    0) op='=';;
+    1) op='>';;
+    2) op='<';;
+esac
+
+if [[ $op != '<' ]]
+then
+    echo "Version number already the latest: $currentver"
+    #exit 0;
+else
+    echo "Newer version available: $newver"
+    rm ${swift_setup_dir}/*.*
+    _download_setup
+    if [ ! $? == 0 ]; then
+        exit 1;
+    fi
+    # and we end up by setting the current version.
+    echo $newver > ${swift_current_version_dir}/version.txt
+    exit 0;
+fi
