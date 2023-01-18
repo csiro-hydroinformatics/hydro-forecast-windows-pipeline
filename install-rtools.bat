59592531cb37bac1bf3b0342f308b3d774ff860e
J-M <jean-michel.perraud@csiro.au>
2023-01-18 15:13:28 +1100

    update version of rtools downloaded

--- install-rtools.bat
+++ install-rtools.bat
@@ -25,17 +25,25 @@ if not exist %download_dir% (
 
 cd %download_dir%
 
-curl -o rtools42-5253-5107-signed.exe https://cran.csiro.au/bin/windows/Rtools/rtools42/files/rtools42-5253-5107-signed.exe
+@REM Ideally should do something like this to get the exact version:
+:: curl -o blah.html https://cran.csiro.au/bin/windows/Rtools/rtools42/files/
+:: more blah.html | grep .exe | sed -r 's/.*href="([^"]+).*/\1/g'
+@REM BUT this is an inane pain to set to a variable in DOS, and cannot test easily. Later.
+@REM https://stackoverflow.com/questions/108439/how-do-i-get-the-result-of-a-command-in-a-variable-in-windows
+
+@REM as of Jan 2023: rtools42-5355-5357.exe
+
+curl -o rtools42-5355-5357-signed.exe https://cran.csiro.au/bin/windows/Rtools/rtools42/files/rtools42-5355-5357-signed.exe
 if %errorlevel% neq 0 (
     set exit_code=%errorlevel%
-    set error_msg=rtools42-5253-5107-signed.exe download failed
+    set error_msg=rtools42-5355-5357-signed.exe download failed
     goto exit
 )
 
-rtools42-5253-5107-signed.exe  /VERYSILENT /SUPRESSMSGBOXES /NORESTART /ALLUSERS
+rtools42-5355-5357-signed.exe  /VERYSILENT /SUPRESSMSGBOXES /NORESTART /ALLUSERS
 if %errorlevel% neq 0 (
     set exit_code=%errorlevel%
-    set error_msg=FAILED: rtools42-5253-5107-signed.exe  /VERYSILENT /SUPRESSMSGBOXES /NORESTART /ALLUSERS
+    set error_msg=FAILED: rtools42-5355-5357-signed.exe  /VERYSILENT /SUPRESSMSGBOXES /NORESTART /ALLUSERS
     goto exit
 )
 
