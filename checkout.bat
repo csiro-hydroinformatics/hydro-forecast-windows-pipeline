6dd9c69bcf90d922c978e41ceb142357028d87c0
J-M <jean-michel.perraud@csiro.au>
2023-11-13 15:36:14 +1100

    WIP: trying to use commit hashes to check out at specific commits.

--- checkout.bat
+++ checkout.bat
@@ -48,11 +48,43 @@ set GITHUB_REPOS=%OUR_SRC_DIR%
 
 set exit_code=0
 
+cd %GITHUB_REPOS%
+if not exist sf-stack git clone %GITHUB_REPO_ROOT%/sf-stack
+
+cd %GITHUB_REPOS%\sf-stack
+git checkout %BRANCH_NAME_ENV_VAR%
+call %GITHUB_REPOS%\sf-stack\commit_hashes.bat
+
+:: turn the detached message off
+git config --global advice.detachedHead false
+
+
+:: REPO_CRUISE_CONTROL
+:: REPO_NUMERICAL_SL_CPP
+:: REPO_DATATYPES
+:: REPO_SWIFT
+:: REPO_QPP
+:: REPO_CHYPP
+:: REPO_VCPP_COMMONS
+:: REPO_MOIRAI
+:: REPO_C_INTEROP
+:: REPO_PYREFCOUNT
+:: REPO_THREADPOOL
+:: REPO_CONFIG_UTILS
+:: REPO_WILA
+:: REPO_EFTS
+:: REPO_EFTS_PYTHON
+:: REPO_MHPLOT
+
 cd %CSIRO_BITBUCKET%
 if not exist cruise-control git clone %CSIRO_BITBUCKET_URL_ROOT%/%REMOTE_REPO_CSIRO%/cruise-control.git
+cd %CSIRO_BITBUCKET%\cruise-control
+git checkout %REPO_CRUISE_CONTROL%
 
 cd %GITHUB_REPOS%
 if not exist vcpp-commons git clone %GITHUB_REPO_ROOT%/vcpp-commons
+cd %GITHUB_REPOS%\vcpp-commons
+git checkout %REPO_VCPP_COMMONS%
 
 @REM echo "TEMP TEST: just check out cruise control, then exit."
 @REM set exit_code=0
@@ -65,19 +97,29 @@ if %errorlevel% neq 0 (
     set exit_code=1
     goto exit
 )
+cd %CSIRO_BITBUCKET%\numerical-sl-cpp
+git checkout %REPO_NUMERICAL_SL_CPP%
 
 
 cd %CSIRO_BITBUCKET%
 if not exist datatypes git clone %CSIRO_BITBUCKET_URL_ROOT%/%REMOTE_REPO_CSIRO%/datatypes.git
+cd %CSIRO_BITBUCKET%\datatypes
+git checkout %REPO_DATATYPES%
 
 cd %CSIRO_BITBUCKET%
 if not exist swift git clone %CSIRO_BITBUCKET_URL_ROOT%/%REMOTE_REPO_CSIRO%/swift.git
+cd %CSIRO_BITBUCKET%\swift
+git checkout %REPO_SWIFT%
 
 cd %CSIRO_BITBUCKET%
 if not exist qpp git clone %CSIRO_BITBUCKET_URL_ROOT%/%REMOTE_REPO_CSIRO%/qpp.git
+cd %CSIRO_BITBUCKET%\qpp
+git checkout %REPO_QPP%
 
 cd %CSIRO_BITBUCKET%
 if not exist chypp git clone %CSIRO_BITBUCKET_URL_ROOT%/%REMOTE_REPO_CSIRO%/CHyPP.git
+cd %CSIRO_BITBUCKET%\chypp
+git checkout %REPO_CHYPP%
 
 cd %GITHUB_REPOS%
 if not exist moirai (
@@ -88,90 +130,59 @@ if not exist moirai (
         goto exit
     )
 )
+cd %GITHUB_REPOS%\moirai
+git checkout %REPO_MOIRAI%
 
 cd %GITHUB_REPOS%
 if not exist c-interop git clone %GITHUB_REPO_ROOT%/c-interop.git
+cd %GITHUB_REPOS%\c-interop
+git checkout %REPO_C_INTEROP%
 
 cd %GITHUB_REPOS%
 if not exist threadpool git clone %GITHUB_REPO_ROOT%/threadpool.git
+cd %GITHUB_REPOS%\threadpool
+git checkout %REPO_THREADPOOL%
 
 cd %GITHUB_REPOS%
 if not exist wila git clone %GITHUB_REPO_ROOT%/wila.git
+cd %GITHUB_REPOS%\wila
+git checkout %REPO_WILA%
 
 cd %GITHUB_REPOS%
 if not exist config-utils git clone %GITHUB_REPO_ROOT%/config-utils.git
+cd %GITHUB_REPOS%\config-utils
+git checkout %REPO_CONFIG_UTILS%
 
 :: # OPTIONAL if needed to generate code for e.g. R package:
 @REM git clone %GITHUB_REPO_ROOT%/c-api-wrapper-generation.git
 
 cd %GITHUB_REPOS%
 if not exist yaml-cpp git clone %GITHUB_JM_REPO_ROOT%/yaml-cpp.git
+cd %GITHUB_REPOS%\yaml-cpp
+git checkout yaml_swift_experimental
 
 cd %GITHUB_REPOS%
 if not exist jsoncpp git clone %GITHUB_JM_REPO_ROOT%/jsoncpp.git
+cd %GITHUB_REPOS%\jsoncpp
+git checkout custom/experimental
 
 cd %GITHUB_REPOS%
 if not exist efts git clone %GITHUB_REPO_ROOT%/efts.git
+cd %GITHUB_REPOS%\efts
+git checkout %REPO_EFTS%
 
 cd %GITHUB_REPOS%
 if not exist efts-python git clone %GITHUB_REPO_ROOT%/efts-python.git
+cd %GITHUB_REPOS%\efts-python
+git checkout %REPO_EFTS_PYTHON%
 
 cd %GITHUB_REPOS%
 if not exist mhplot git clone %GITHUB_REPO_ROOT%/mhplot.git
+cd %GITHUB_REPOS%\mhplot
+git checkout %REPO_MHPLOT%
 
 cd %GITHUB_REPOS%
 if not exist pyrefcount git clone %GITHUB_REPO_ROOT%/pyrefcount.git
-
-if not exist numerical-sl-cpp (
-    cd %CSIRO_BITBUCKET%\numerical-sl-cpp
-    git checkout testing
-    if %errorlevel% neq 0 ( 
-        echo "ERROR: numerical-sl-cpp branch checkout failed"
-        set exit_code=1
-        goto exit
-    )
-)
-
-cd %CSIRO_BITBUCKET%\numerical-sl-cpp
-git checkout testing
-cd %CSIRO_BITBUCKET%\datatypes
-git checkout testing
-cd %CSIRO_BITBUCKET%\swift
-git checkout testing
-cd %CSIRO_BITBUCKET%\qpp
-git checkout testing
-cd %CSIRO_BITBUCKET%\chypp
-git checkout testing
-
-cd %CSIRO_BITBUCKET%\cruise-control
-git checkout testing
-
-cd %GITHUB_REPOS%\vcpp-commons
-git checkout testing
-
-cd %GITHUB_REPOS%\moirai
-git checkout testing
-cd %GITHUB_REPOS%\c-interop
-git checkout testing
-cd %GITHUB_REPOS%\threadpool
-git checkout master
-cd %GITHUB_REPOS%\wila
-git checkout testing
-cd %GITHUB_REPOS%\config-utils
-git checkout testing
-cd %GITHUB_REPOS%\yaml-cpp
-git checkout yaml_swift_experimental
-cd %GITHUB_REPOS%\jsoncpp
-git checkout custom/experimental
-cd %GITHUB_REPOS%\efts
-git checkout testing
-cd %GITHUB_REPOS%\efts-python
-git checkout testing
-cd %GITHUB_REPOS%\mhplot
-git checkout master
-cd %GITHUB_REPOS%\pyrefcount
-git checkout testing
-
 
 @REM cd %CSIRO_BITBUCKET%
 @REM git clone %CSIRO_BITBUCKET_URL_ROOT%/%REMOTE_REPO_CSIRO%/rpp-cpp.git
