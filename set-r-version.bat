e3403d407629f32ec0a271dc9290be3da6fadd90
J-M <jean-michel.perraud@csiro.au>
2026-04-27 15:58:16 +1000

    fix: R version on windows 2022 image has moved up

--- set-r-version.bat
+++ set-r-version.bat
@@ -2,8 +2,8 @@
 @set exit_code_setrv=0
 @set error_msg=""
 
-@REM as of Feb 2026 windows 2022
-set R_VERSION=4.5.2
+@REM as of Apr 2026 windows 2022
+set R_VERSION=4.5.3
 
 set R_PROG_DIR=c:\Program Files\R
 set R_EXE="%R_PROG_DIR%\R-%R_VERSION%\bin\x64\R.exe"
