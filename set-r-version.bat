ae5bedd5d1203e1039ba3507470b0f7e919874b1
J-M <jean-michel.perraud@csiro.au>
2026-09-02 14:39:35 +1000

    chore: bump R version on azure image

--- set-r-version.bat
+++ set-r-version.bat
@@ -2,15 +2,15 @@
 @set exit_code_setrv=0
 @set error_msg=""
 
-@REM as of May 2026 windows 2022
-set R_VERSION=4.6.0
+@REM as of Sept 2026 windows 2022  https://github.com/actions/runner-images/blob/main/images/windows/Windows2022-Readme.md
+set R_VERSION=4.6.1
 
 set R_PROG_DIR=c:\Program Files\R
 set R_EXE="%R_PROG_DIR%\R-%R_VERSION%\bin\x64\R.exe"
 set R_SCRIPT="%R_PROG_DIR%\R-%R_VERSION%\bin\x64\Rscript.exe"
 
 if not exist %R_EXE% (
-    set error_msg="ERROR: path '%R_EXE%' does not exist"
+    set error_msg="ERROR: path '%R_EXE%' does not exist. Check https://github.com/actions/runner-images/blob/main/images/windows/Windows2022-Readme.md"
     set exit_code_setrv=1
     goto exit
 )
